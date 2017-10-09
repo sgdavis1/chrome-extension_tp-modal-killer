fc72102396422677003e47e8d7556a4ca4ab5021
Steven Davis <sgdavis@bioneos.com>
2017-10-09 09:26:34 -0500

    Added Washington Post support

--- killer.js
+++ killer.js
@@ -1,11 +1,16 @@
 try
 {
+  // TP modal type
   $('.tp-modal').remove();
   $('.tp-backdrop').remove();
   $('body').removeClass('tp-modal-open');
+
+  // Washington post
+  $('div[class*="Modal"]').remove();
+  $('html').removeClass('drawbridge-up');
 }
 catch (e)
 {
-  alert('Could not remove tp-modal: ' + e.message);
+  alert('Could not remove WashPost / tp-modal: ' + e.message);
 }
 
